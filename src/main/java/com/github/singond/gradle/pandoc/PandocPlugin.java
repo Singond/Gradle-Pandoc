47005c97ba16b3c1862e6a43657f06c5d95b81ad
Singon <singond@seznam.cz>
2018-12-16 18:34:27 +0100

    Transform into plugin

---
+++ src/main/java/com/github/singond/gradle/pandoc/PandocPlugin.java
@@ -0,0 +1,13 @@
+package com.github.singond.gradle.pandoc;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class PandocPlugin implements Plugin<Project> {
+
+	@Override
+	public void apply(Project project) {
+		project.getTasks().create("pandoc", PandocTask.class);
+	}
+
+}
