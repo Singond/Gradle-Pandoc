fadae3a4894ba505e7f0e5f830870714e1a62ee1
Singon <singond@seznam.cz>
2018-12-17 14:39:10 +0100

    Test source files traversal

--- src/main/java/com/github/singond/gradle/pandoc/PandocTask.java
+++ src/main/java/com/github/singond/gradle/pandoc/PandocTask.java
@@ -1,12 +1,58 @@
 package com.github.singond.gradle.pandoc;
 
+import java.io.File;
+
 import org.gradle.api.DefaultTask;
+import org.gradle.api.file.FileCollection;
+import org.gradle.api.tasks.InputFiles;
+import org.gradle.api.tasks.OutputDirectory;
+import org.gradle.api.tasks.SkipWhenEmpty;
 import org.gradle.api.tasks.TaskAction;
 
 public class PandocTask extends DefaultTask {
 
+	private FileCollection sources;
+	private File outputDir;
+
+	@InputFiles
+	@SkipWhenEmpty
+	public FileCollection getSources() {
+		return sources;
+	}
+
+	public void sources(FileCollection sources) {
+		this.sources = sources;
+	}
+
+	@OutputDirectory
+	public File getOutputDir() {
+		return outputDir;
+	}
+
+	public void outputDir(File ouputDir) {
+		this.outputDir = ouputDir;
+	}
+
+	// Testing only
+	public void printContents() {
+		System.out.println("Sources: " + sources);
+		for (File s : sources) {
+			System.out.println("Contains: " + s);
+		}
+		System.out.println("Sources as files: " + sources.getFiles());
+		for (File s : sources.getFiles()) {
+			System.out.println("Contains: " + s);
+		}
+		System.out.println("Sources as tree: " + sources.getAsFileTree());
+		for (File s : sources.getAsFileTree()) {
+			System.out.println("Contains: " + s);
+		}
+	}
+
 	@TaskAction
 	public void executeTask() {
-		System.out.println("Executing Pandoc task");
+		for (File s : sources.getAsFileTree()) {
+			System.out.println("Processing: " + s);
+		}
 	}
 }
---
+++ test/docs/test.md
@@ -0,0 +1,12 @@
+Testing the Pandoc plugin for Gradle
+====================================
+This is a testing file to be processed by the Pandoc plugin. It is written in
+markdown and this task will attempt to convert it to other output formats.
+
+Random text
+-----------
+Tenetur et non quo. Perspiciatis modi dolor recusandae. Et doloribus eius
+consequatur minus similique necessitatibus. Omnis aut praesentium modi ipsam
+tenetur sequi. Libero molestiae accusantium modi voluptatibus autem doloremque
+exercitationem. Quos sed aut aut cupiditate ipsa. Sapiente ducimus non et et
+vero. Qui cum repellendus enim modi tenetur accusamus.
